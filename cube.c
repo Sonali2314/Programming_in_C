358bba1a9070112f7e5d47ef08787016536906e6
Sonali Patil <118295011+Sonali2314@users.noreply.github.com>
2024-02-17 22:23:28 +0530

    cube.c

---
+++ cube.c
@@ -0,0 +1,15 @@
+//Write a Prograam to print cube of the given number . 
+#include<stdio.h> 
+int main() 
+{ 
+ int n,cube; 
+
+ printf("Enter any integer Value : "); 
+ scanf("%d",&n); 
+ 
+ cube=n*n*n; 
+ 
+ printf("\n Cube of a given number %d is = %d", n, cube); 
+ 
+ return 0; 
+}
