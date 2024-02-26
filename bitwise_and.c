79975874091b29ce16002e499f7d98efa0b9b2e1
Sonali Patil <118295011+Sonali2314@users.noreply.github.com>
2024-02-26 11:16:29 +0530

    bitwise_and.c

---
+++ bitwise_and.c
@@ -0,0 +1,12 @@
+//Write a Bitwise AND of two numbers . 
+#include <stdio.h> 
+int main() { 
+ int a, b; 
+ printf("Enter the first integer: "); 
+ scanf("%d", &a); 
+ printf("Enter the second integer: "); 
+ scanf("%d", &b); 
+ int result = a & b; 
+ printf("Binary AND result: %d\n", result); 
+ return 0; 
+} 
